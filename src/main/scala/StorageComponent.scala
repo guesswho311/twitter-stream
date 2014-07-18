ee674efa57a5d332d8a306952224f25ba5ff6173
Jonathan Quist <jonathan.quist@banno.com>
2014-07-17 19:53:20 -0500

    small refactor, abstracted out a method

--- src/main/scala/StorageComponent.scala
+++ src/main/scala/StorageComponent.scala
@@ -17,12 +17,11 @@ trait AkkaStorageComponent extends StorageComponent with AkkaSystem{
     var hashtags: List[String] = List()
     var emojis: List[String] = List()
 
-    def getTopItem(ls: List[String]) =
-      ls.groupBy(identity).mapValues(_.size).maxBy(i => i._2)._1
-
     def receive: Receive = {
       case hosts: Hosts => hostAddresses = hostAddresses ++ hosts.urls
+
       case h: HashTags => hashtags = hashtags ++ h.tags
+
       case emoticons:Emojis => emojis = emojis ++ emoticons.e
 
       case TweetReport =>
@@ -30,25 +29,31 @@ trait AkkaStorageComponent extends StorageComponent with AkkaSystem{
         println("Total tweets processed: " + TweetHealth.getTotalTweets)
         println("Tweets per second: " + Math.round(TweetHealth.getTweetRate))
 
-        val topEmoji = emojis.groupBy(identity).mapValues(_.size).maxBy(i => i._2)._1
+        val topEmoji = getTopItem(emojis)
         val totalEmojiTweets = TweetHealth.getTotalEmoTweets
         println("Percentage of tweets with emojis: " + (totalEmojiTweets.toDouble / TweetHealth.getTotalTweets.toDouble) * 100)
         println("Top emoji: " + topEmoji)
 
-        val topHashTag = hashtags.groupBy(identity).mapValues(_.size).maxBy(i => i._2)._1
+        val topHashTag = getTopItem(hashtags)
         println("Top hashtag in tweets: " + topHashTag)
 
         println("Percentage of tweets with urls: " + (TweetHealth.getTotalUrlTweets.toDouble / TweetHealth.getTotalTweets.toDouble) * 100)
         println("Percentage of tweets with photos: " + (TweetHealth.getTotalPhotoUrlTweets.toDouble / TweetHealth.getTotalTweets.toDouble) * 100)
 
-        val topDomain = hostAddresses.groupBy(identity).mapValues(_.size).maxBy(i => i._2)._1
+        val topDomain = getTopItem(hostAddresses)
         println("Top host in tweets: " + topDomain)
         println("--------------------- End Transmission -----------------------------")
         println()
 
-        context.system.scheduler.scheduleOnce(FiniteDuration(5, TimeUnit.SECONDS)){
-          self ! TweetReport
-        }
+        scheduleTweetReport(5)
+    }
+
+    def getTopItem(ls: List[String]) =
+      ls.groupBy(identity).mapValues(_.size).maxBy(i => i._2)._1
+
+    def scheduleTweetReport(time: Int) =
+      context.system.scheduler.scheduleOnce(FiniteDuration(time, TimeUnit.SECONDS)){
+        self ! TweetReport
+      }
   }
-}
 }
